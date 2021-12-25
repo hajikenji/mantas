f982f506cee5291625d1cd5d6028371b3c46ec2e
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-25 22:01:50 +0900

    taskのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :name, presenece: true
+  validates :content, presenece: true
+  validates :priority, presenece: true
+  validates :status, presenece: true
 end
