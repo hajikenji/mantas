179e89af435466f239931c8a08be4f786d7250fc
kenji hajiro <mademoiselle3824@gmail.com>
2021-12-25 21:29:24 +0900

    has_secureのgem、userのバリデーション、ER図

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: { maximum:20 }
+  validates :email, presence: true, length: { maximum:255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: true
+  validates :password, length: { maximum: 4 }
 end
