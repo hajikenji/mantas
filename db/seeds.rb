6f777ddde491c2ec59c05961c9fb5ccd72c7b968
kenji hajiro <mademoiselle3824@gmail.com>
2022-01-18 11:08:32 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-15.times do |n|
+10.times do |n|
   name = Faker::Games::SuperSmashBros.fighter
   email = Faker::Internet.email
   password = "dddd"
@@ -13,5 +13,35 @@
                name: name,
                email: email,
                password: password
+  )
+end
+
+10.times do |n|
+  name = Faker::Games::SuperSmashBros.fighter
+  Label.create!(
+               label_name: name
+  )
+end
+
+
+
+10.times do |n|
+  a = User.all.size
+  user = User.all[rand(0..a)]
+
+  name = Faker::Games::SuperSmashBros.fighter
+  content = "aaa"
+  time = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+  status = rand(1..3)
+  priority = status + 3
+  label_ids = rand(Label.first[:id]..Label.first[:id] + Label.all.size)
+  Task.create!(
+               name: name,
+               content: content,
+               time: time,
+               status: status,
+               priority: priority,
+               label_ids: label_ids,
+               user_id: user.id
   )
 end
